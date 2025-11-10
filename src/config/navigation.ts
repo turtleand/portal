b9bb5b5538c0b8fe9d2fb3c95fd67aaa7cbe3ec9
Turtleand <turtliophant@gmail.com>
2025-11-09 23:01:48 -0300

    Remove LinkedIn social

---
+++ src/config/navigation.ts
@@ -0,0 +1,8 @@
+export type NavigationItem = {
+  label: string;
+  href: string;
+};
+
+export const primaryNavigation: NavigationItem[] = [
+  { label: 'About', href: '/about' },
+];
---
+++ src/config/socials.ts
@@ -0,0 +1,26 @@
+export type SocialLink = {
+  name: 'Github' | 'Mail';
+  href: string;
+  linkTitle: string;
+  icon: 'github' | 'mail';
+  active: boolean;
+};
+
+const siteTitle = 'Turtleand';
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    name: 'Github',
+    href: 'https://github.com/turtlean',
+    linkTitle: `${siteTitle} on Github`,
+    icon: 'github',
+    active: true,
+  },
+  {
+    name: 'Mail',
+    href: 'mailto:hello@turtleand.com',
+    linkTitle: `Send an email to ${siteTitle}`,
+    icon: 'mail',
+    active: true,
+  },
+];
